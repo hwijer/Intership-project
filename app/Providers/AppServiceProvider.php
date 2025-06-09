7cca1b5f632c5563c7e27c16150334f14636cf59
hwijer <hajarladdad04@gmail.com>
2025-06-09 19:28:24 +0100

    FEAT: bind IAdminService to AdminService in AppServiceProvider for service layer abstraction

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\AdminService;
+use App\Services\Contracts\IAdminService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(IAdminService::class, AdminService::class); // everytime bind interface of a service layer, so u can abstract ur business login from the controller.
+        // You can bind other services here as needed
+        // $this->app->bind(IExampleService::class, ExampleService::class); // Example for another service binding 'I' refer to interface
     }
 
     /**
